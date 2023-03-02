991c1b4504643fffbb2a6c64455135fd46585e1b
Marat Mikaelyan <MMikaelyan@alfabank.ru>
2023-03-02 20:10:37 +0300

    Initial commit

---
+++ ScoreCounter/ScoreCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreCounterApp.swift
+//  ScoreCounter
+//
+//  Created by Marat Mikaelyan on 02.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScoreCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
