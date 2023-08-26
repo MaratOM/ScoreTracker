606de9f5e30ee7baf96a126614e93216bcca14e8
Marat Mikaelyan <MMikaelyan@alfabank.ru>
2023-08-26 20:15:08 +0300

    Refactors Add Item view

--- ScoreCounter/Common/Views/AddItemView.swift
+++ ScoreCounter/Common/Views/AddItemView.swift
@@ -9,21 +9,16 @@ import SwiftUI
 
 struct AddItemView: View {
     var linkView: any View
+    let iconSize: CGFloat = 50
     
     var body: some View {
         NavigationLink {
             AnyView(linkView)
         } label: {
-            HStack {
-                Spacer()
-                Label("text", systemImage: "plus.circle")
-                    .font(.system(size: 50))
-                    .foregroundColor(.black)
-                    .labelStyle(.iconOnly)
-                    .padding([.trailing], 30)
-                    .padding([.bottom], 20)
-
-            }
+            Label("text", systemImage: "plus.circle")
+                .font(.system(size: iconSize))
+                .foregroundColor(.blue)
+                .labelStyle(.iconOnly)
         }
     }
 }
--- ScoreCounter/Games/Screens/GamesScreen.swift
+++ ScoreCounter/Games/Screens/GamesScreen.swift
@@ -6,50 +6,38 @@
 //
 
 import SwiftUI
-import UIKit
 
 struct GamesScreen: View {
     @EnvironmentObject var store: ScoreCounterStore
-    @State var pushActive = false
-    @State var text = "Go to First Player"
 
     var body: some View {
         let games = store.games
 
         NavigationView {
             VStack {
-                List(games) { game in
-                    NavigationLink {
-                        Text(game.name)
-                            .font(.system(size: 40))
-                    } label: {
-                        Label(game.name, systemImage: game.type.imageName)
-                    }
-                }
+                let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 1)
                 
-                HStack {
-                    UIKitLabel(text: $text)
-                        .onTapGesture {
-                            store.tabSelection = .players
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-                                pushActive.toggle()
+                ScrollView {
+                    LazyVGrid(columns: columns, alignment: .leading) {
+                        ForEach(games) { game in
+                            NavigationLink {
+                                Text(game.name)
+                                    .font(.system(size: 40))
+                            } label: {
+                                Label(game.name, systemImage: game.type.imageName)
+                                Spacer()
+                                Label("", systemImage: "chevron.right")               
                             }
+                            .padding(.bottom, 10)
                         }
-                    AddItemView(linkView: AddGameScreen())
+                    }
+                    .padding()
                 }
-                .padding(.leading, 20)
-                .frame(width: nil, height: 70)
-                
-                NavigationLink(
-                    destination: PlayerScreen(player: store.players[0]),
-                    isActive: self.$pushActive
-                ) {
-                    EmptyView()
-                }.hidden()
+
+                AddItemView(linkView: AddGameScreen())
             }
             .navigationTitle("Games")
         }
-        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
@@ -57,17 +45,5 @@ struct GamesScreen_Previews: PreviewProvider {
     static var previews: some View {
         GamesScreen()
             .environmentObject(ScoreCounterStore())
-    }
-}
-
-struct UIKitLabel: UIViewRepresentable {
-    @Binding var text: String
-    
-    func makeUIView(context: Context) -> UILabel {
-        UILabel()
-    }
-    
-    func updateUIView(_ uiView: UILabel, context: Context) {
-        uiView.text = text
     }
 }
